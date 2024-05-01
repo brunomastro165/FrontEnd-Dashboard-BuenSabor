0faf1f7a2eab5817635c255cc3b8be634cc88aa2
Bruno Mastropietro <brunomastro165@gmail.com>
2024-05-01 00:50:30 -0300

    Pude hacer las categorías recursivas

--- src/components/ui/TableCategorias/ContainerCategorias.tsx
+++ src/components/ui/TableCategorias/ContainerCategorias.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BackendClient } from '../../../services/BackendClient'
+import { ICategoria } from '../../../types/Categoria';
+import CategoriaButton from './Buttons/CategoriaButton';
+
+class CRUDMetods extends BackendClient<any> { }
 
 const ContainerCategorias = () => {
+
+  const backend = new CRUDMetods();
+
+  const [categorias, setCategorias] = useState<ICategoria[]>([]);
+
+  useEffect(() => {
+    const getCategorias = async () => {
+      const res: ICategoria[] = await backend.getAll("http://localhost:8080/categorias");
+      setCategorias(res);
+      console.log(res)
+    }
+    getCategorias();
+  }, [])
+
+
   return (
-    <div>ContainerCategorias</div>
+    <div className=' mx-auto w-1/2'>
+      {categorias.map((categoria) => (
+        <>
+          <h1 className='font-Roboto text-xl'>{categoria.denominacion}</h1>
+          <div className='border rounded  p-5 m-5'>
+            <CategoriaButton categoria={categoria} />
+          </div>
+        </>
+      ))}
+
+
+    </div>
   )
 }
 
