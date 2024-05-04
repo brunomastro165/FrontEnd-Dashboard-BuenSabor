f57ac2d92f72d20d4c60ac97a18c65f128d33e9a
brunomastro165 <brunomastro165@gmail.com>
2024-05-04 15:22:19 -0300

    Articulos manufacturados ahora son mapeados por sucursal

--- src/components/screens/Productos/Manufacturados.tsx
+++ src/components/screens/Productos/Manufacturados.tsx
@@ -10,12 +10,26 @@ import useFilteredData from '../../../hooks/useFilteredData';
 import BasePage from './BasePage';
 import { fetchData } from '../../api/Fetch';
 import { IArticuloManufacturado } from '../../../types/ArticuloManufacturado';
+import { BackendClient } from '../../../services/BackendClient';
+import { IEmpresa } from '../../../types/Empresa';
+import { ISucursal } from '../../../types/Sucursal';
+import { ICategoria } from '../../../types/Categoria';
+
+class Backend extends BackendClient<T> { }
 
 const Manufacturados = () => {
+
+    const backend = new Backend();
+
+    const { idEmpresa, idSucursales } = useParams();
 
     const [loading, setLoading] = useState<boolean>(false);
 
     const [data, setData] = useState<IItem[]>([]);
+
+    const [todosLosArticulos, setTodosLosArticulos] = useState<any[]>([]);
+
+    //const [categorias, setCategorias] = useState<ICategoria[]>([])
 
     //Esto es para normalizar los datos de IArticuloInsumo que traiga el Fetch, asi la tabla puede entender
     //distintos tipos de datos.
@@ -30,13 +44,55 @@ const Manufacturados = () => {
         }));
     }
 
-    // Uso de la función
+    //CODIGO RECURSIVO PARA TRAER TODOS LOS PRODUCTOS DE UNA SUCURSAL
+
+    const obtenerArticulos = (categoria: ICategoria): any[] => {
+
+        let articulos = [...categoria.articulos];
+
+        categoria.subCategorias.forEach(subCategoria => {
+            articulos = [...articulos, ...obtenerArticulos(subCategoria)];
+        });
+
+        return articulos;
+    }
+
     useEffect(() => {
         const fetchManufacturado = async () => {
-            const response = await fetchData("http://localhost:8080/articulosManufacturados");
-            const transformedData = transformData(response);
-            setData(transformedData);
-            setLoading(true);
+
+            //Esto está hecho de maneria precaria debido a las limitaciones de JSON SERVER,
+            // con un backend funcional se hará el endpoint correspondiente
+
+            if (idEmpresa && idSucursales) {
+
+                //Buscamos la empresa por el ID que nos traemos con useParams
+                const idEmpresaString = idEmpresa.toString()
+                const response: IEmpresa = await backend.get("http://localhost:8080/empresas", idEmpresaString)
+
+                //Una vez dentro de la empresa, buscamos la sucursal seleccionada con useParams
+                const idSucursalNumber = Number(idSucursales)
+                const sucursal: ISucursal | undefined = response.sucursales.find((sucursal: ISucursal) => sucursal.id === idSucursalNumber)
+
+                //Ahora buscamos las categorías dentro de la sucursal que fué seleccionada
+                const categorias: ICategoria[] | undefined = sucursal?.categorias
+
+                //Llamada a la función obtener articulos, la cual nos retorna todos los articulosManufacturados dentro de todas las categorías
+                const arCategorias: ICategoria[] | undefined = categorias;
+                let articulos: IArticuloManufacturado[] = [];
+
+                arCategorias?.forEach(categoria => {
+                    articulos = [...articulos, ...obtenerArticulos(categoria)];
+                });
+
+                //Llamada a la función transformData, la cual nos retorna los datos de articuloManufacturado en un tipo de dato
+                //compatible con la el componente Table
+
+                console.log(articulos)
+                const transformedData = transformData(articulos);
+                setData(transformedData);
+
+                setLoading(true);
+            }
         }
         fetchManufacturado();
     }, [loading])
